cfc917c2a187768dd51158277356878576af7884
soumen-pradhan <sam.thepradhans@gmail.com>
2025-12-29 21:47:40 +0530

    Simple qol changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,54 @@
-#include <glad/glad.h>
+#include <stdint.h>
+
+#include <glad/glad.h> // glad has to be above glfw3 header
 
 #include <GLFW/glfw3.h>
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 #include <spdlog/spdlog.h>
+
+#include "defer.h"
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
-    spdlog::set_level(spdlog::level::debug);
-    spdlog::set_pattern(
-        "%Y-%m-%d %H:%M:%S.%e "
-        "%^%5l%$ | %s:%# | %v");
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::set_pattern("%Y-%m-%d %H:%M:%S.%e %^%5l%$ | %s:%# | %v");
 
     if (!glfwInit()) {
         SPDLOG_ERROR("Failed to init GLFW");
-        return -1;
+        return 1;
     }
+    DEFER(glfwTerminate());
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Hello World", nullptr, nullptr);
+    const int32_t SCR_WIDTH = 800;
+    const int32_t SCR_HEIGHT = 600;
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL Hello World", nullptr, nullptr);
     if (!window) {
         SPDLOG_ERROR("Failed to create window");
-        glfwTerminate();
-        return -1;
+        return 1;
     }
+    DEFER(glfwDestroyWindow(window));
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         SPDLOG_ERROR("Failed to init GLAD");
-        return -1;
+        return 1;
     }
 
     SPDLOG_INFO("OpenGL {}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
 
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -43,7 +56,20 @@ int main()
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
     return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        SPDLOG_DEBUG("Pressed Esc Key");
+        glfwSetWindowShouldClose(window, true);
+    }
 }
