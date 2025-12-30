b9c18cbb2721fe33cccef744d0167862df200275
soumen-pradhan <sam.thepradhans@gmail.com>
2025-12-30 22:30:53 +0530

    Ex 5.8.1

--- src/main.cpp
+++ src/main.cpp
@@ -55,17 +55,21 @@ int main()
     uint32_t vertexArrayObj = 0;
     uint32_t elementBufferObj = 0;
     {
+        // clang-format off
         float vertices[] = {
-            0.5f, 0.5f, 0.0f, // top right
-            0.5f, -0.5f, 0.0f, // bottom right
-            -0.5f, -0.5f, 0.0f, // bottom left
-            -0.5f, 0.5f, 0.0f // top left
+            0.1f, 0.0f, 0.0f,
+            0.3f, 0.0f, 0.0f,
+            0.2f, 0.2f, 0.0f,
+            -0.1f, 0.0f, 0.0f,
+            -0.3f, 0.0f, 0.0f,
+            -0.2f, -0.2f, 0.0f,
         };
 
         uint32_t indices[] = {
-            0, 1, 3,
-            1, 2, 3
+            0, 1, 2,
+            3, 4, 5
         };
+        // clang-format on
 
         glGenBuffers(1, &vertexBufferObj);
         glGenVertexArrays(1, &vertexArrayObj);
